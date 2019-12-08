a06236285bc8e5e912a054ce8b05c4f1f01c083d
andrew54068 <andrew0424718012@yahoo.com.tw>
2019-12-08 22:13:51 +0800

    [feat] add web socket succeed.

--- BinanceTest/TradingViewController.swift
+++ BinanceTest/TradingViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import Starscream
 
 final class TradingViewController: UIViewController {
+    
+    private var socket: WebSocket?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        if let url: URL = URL(string: "wss://stream.binance.com:9443/ws/bnbbtc@depth") {
+            socket = WebSocket(url: url)
+            socket?.delegate = self
+            socket?.connect()
+        }
     }
-
-
+    
 }
 
+extension TradingViewController: WebSocketDelegate {
+    
+    func websocketDidConnect(socket: WebSocketClient) {
+        
+    }
+    
+    func websocketDidDisconnect(socket: WebSocketClient, error: Error?) {
+        
+    }
+    
+    func websocketDidReceiveMessage(socket: WebSocketClient, text: String) {
+        
+    }
+    
+    func websocketDidReceiveData(socket: WebSocketClient, data: Data) {
+        
+    }
+    
+}

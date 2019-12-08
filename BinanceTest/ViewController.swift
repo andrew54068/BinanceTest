b45e27aade66a851ecac6633e4e3b255db6da7ae
andrew54068 <andrew0424718012@yahoo.com.tw>
2019-12-08 22:01:31 +0800

    Initial Commit

---
+++ BinanceTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinanceTest
+//
+//  Created by kidnapper on 2019/12/8.
+//  Copyright © 2019 kidnapper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
